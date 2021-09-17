227c3e61d31e2cb4f6f81ae3abe3f0894e278745
hassgang <rikeness@gmail.com>
2021-09-17 22:06:48 +0300

    Revert "Crosshair"
    This reverts commit 6b0ea722b7c885379fdc63f38284d88215056d67.

--- Zombieland/Assets/Scripts/PlayerMovement.cs
+++ Zombieland/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+<<<<<<< Updated upstream
+    private Animator animator;
+    private float horizontal;
+    private float vertical;
+    private float dampTime = 0.1f;
+    private Vector3 direction;
+    private Camera cam;
+    [SerializeField][Range(0f,10f)]private float movementSpeed;
+    [SerializeField]private LayerMask aimLayerMask;
+=======
     private Animator _animator;
     private float _horizontal;
     private float _vertical;
@@ -21,26 +31,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private LayerMask _aimLayerMask;
 
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
-    private void Awake()
+    void Awake()
     {
-        _camera = Camera.main;
-        _animator = GetComponent<Animator>();
+        cam = Camera.main;
+        animator = GetComponent<Animator>();
     }
 
+<<<<<<< Updated upstream
+    // Update is called once per frame
+    void Update()
+=======
     private void Update()
+>>>>>>> Stashed changes
     {
         Move();
         AimTowardsMouse();
     }
- 
+    private void FixedUpdate()
+    {
+
+    }
     private void Move()
     {
-        _horizontal = Input.GetAxisRaw("Horizontal");
-        _vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        
 
+        direction = new Vector3(horizontal, 0, vertical);
+        transform.Translate(direction.normalized*movementSpeed*Time.deltaTime,Space.World);
 
+<<<<<<< Updated upstream
+        animator.SetFloat("VelocityZ", vertical,dampTime,Time.deltaTime);
+        animator.SetFloat("VelocityX", horizontal,dampTime,Time.deltaTime);
+=======
         _direction = new Vector3(_horizontal, 0, _vertical);
         transform.Translate(_direction.normalized * _movementSpeed * Time.deltaTime, Space.World);
 
@@ -49,20 +75,25 @@ public class PlayerMovement : MonoBehaviour
 
         _animator.SetFloat("VelocityZ", _velocityZ, _dampTime, Time.deltaTime);
         _animator.SetFloat("VelocityX", _velocityX, _dampTime, Time.deltaTime);
+>>>>>>> Stashed changes
     }
     private void AimTowardsMouse()
     {
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, _aimLayerMask))
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out RaycastHit hitInfo, Mathf.Infinity, aimLayerMask))
         {
             var destination = hitInfo.point;
             destination.y = transform.position.y;
+<<<<<<< Updated upstream
+           
+=======
 
             Crosshair.Aim(destination);
 
+>>>>>>> Stashed changes
             Vector3 lookDirection = destination - transform.position;
             lookDirection.Normalize();
-
+            
             transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
         }
     }
